3eceeea65c239166158b5fcaa24ff8f8d5ec2b39
Adam Clater <aclater@gmail.com>
2018-01-10 08:32:25 -0500

    Update states to include PR for Puerto Rico & some minor cleanup / comments

--- application/fema.py
+++ application/fema.py
@@ -2,18 +2,17 @@ import json
 import ssl
 import urllib.request
 
-def disastersearch (state):
 
-# disastersearch takes a two character state code as input and returns a sorted list of counties and # of disasters
-# we do this by searching the OpenFEMA API for the input state and creating a histogram of the county & disaster count
+def disastersearch(state):
+    # disastersearch takes a two character state code as input and returns a sorted list of counties and # of disasters
+    # we do this by searching the OpenFEMA API for the input state and creating a histogram of the county & disaster count
 
-
-    #Define a few variables & set HTTPS Context
+    # Define a few variables & set HTTPS Context
     states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "GU" ]
+              "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+              "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+              "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+              "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "GU", "PR"]
     all_disaster_data = {}
     counties = {}
     # Ignore SSL certificate errors
@@ -21,11 +20,11 @@ def disastersearch (state):
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
 
-    state=state.upper()
-    if state not in states :
-        lst=list()
-        lst=[state, "Not a valid state"]
-        return(lst)
+    state = state.upper()
+    if state not in states:
+        lst = list()
+        lst = [state, "Not a valid state"]
+        return (lst)
 
     else:
 
@@ -38,7 +37,7 @@ def disastersearch (state):
         # ALL option deletes state filter & Retrieves all disasters
 
         parameters = {'$inlinecount': 'allpages', '$filter': "state eq\'" + state + "\'"}
-        #if get_all : parameters = {'$inlinecount': 'allpages'}
+        # if get_all : parameters = {'$inlinecount': 'allpages'}
 
         url = serviceurl + urllib.parse.urlencode(parameters)
         uh = urllib.request.urlopen(url, context=ctx)
@@ -65,22 +64,19 @@ def disastersearch (state):
             # pull Disaster Declarations Summaries into its own dictionary - disasters_dict
             disasters_dict = all_disaster_data['DisasterDeclarationsSummaries']
 
-            # Create histogram of disasters_dict by "State Abbreviation - County Name" in dictionary counties
-            # Useful if pulling all records by eliminating "state eq" in parameters dictionary
+            # Create histogram of disasters_dict in dictionary counties
+            # empty "declaredCountyArea (? State Wide Disasters? ) are omitted
 
             for i in range(len(disasters_dict)):
                 disaster_county = disasters_dict[i]['declaredCountyArea']
-                disaster_state = disasters_dict[i]['state']
                 if not disaster_county:
-                #print(disasters_dict[i]['state'], disasters_dict[i]['id'], disasters_dict[i]['declarationDate'],
-                #      disasters_dict[i]['incidentType'], disasters_dict[i]['title'], "No Declared County Area")
                     continue
                 else:
-                    counties[disaster_county] = counties.get(disaster_county,0) + 1
+                    counties[disaster_county] = counties.get(disaster_county, 0) + 1
 
         # Dump our dictionary counties into lst, reverse sort them by value
-        # then loop through the sorted list and print it out highest to lowest
+        # return lst as output of self
         total_disasters = 0
         lst = list(counties.items())
         lst = sorted(lst, key=lambda x: x[1], reverse=True)
-    return(lst)
+    return (lst)
