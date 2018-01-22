c3795d1bb3ec4ce572ad487e84eece1c4c60b7ba
Adam Clater <aclater@gmail.com>
2018-01-22 15:04:06 +0900

    Remove FEMA Logo

--- application/fema.py
+++ application/fema.py
@@ -12,8 +12,7 @@ def disastersearch(state):
               "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
               "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
               "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-              "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY",
-              "GU", "PR"]
+              "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
     all_disaster_data = {}
     counties = {}
